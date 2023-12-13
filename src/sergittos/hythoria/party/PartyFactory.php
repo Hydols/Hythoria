64df3b69131d7cdfd5bad37870d6e2a12edf231e
Sergittos <sergittoscoc@gmail.com>
2023-12-13 01:09:07 +0100

    Remove heading

--- src/sergittos/hythoria/party/PartyFactory.php
+++ src/sergittos/hythoria/party/PartyFactory.php
@@ -1,9 +1,5 @@
 <?php
-/*
-* Copyright (C) Sergittos - All Rights Reserved
-* Unauthorized copying of this file, via any medium is strictly prohibited
-* Proprietary and confidential
-*/
+
 
 declare(strict_types=1);
 
